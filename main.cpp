60afea397d2107176ba79576c64fe1377cbfa316
Cillié Malan <cilliemalan@gmail.com>
2017-10-18 16:56:38 +0200

    Working access token verification

--- main.cpp
+++ main.cpp
@@ -17,42 +17,64 @@ web::http::client::http_client::~http_client() noexcept {}
 
 std::string api_url = "https://slack.com/api";
 
-pplx::task<bool> test_access(http_client client)
+pplx::task<bool> test_access_token(http_client client, const std::string &access_token)
 {
-    return client.request(
-                     methods::POST,
-                     "auth.test",
-                     "",
-                     "application/x-www-form-urlencoded; charset=utf-8")
+    http_request message(methods::POST);
+    message.set_request_uri("auth.test");
+    message.set_body("", "application/x-www-form-urlencoded");
+    message.headers().add("Authorization", "Bearer " + access_token);
+
+    return client.request(message)
         .then([=](http_response response) {
             return response.extract_json();
         })
         .then([=](json::value json) {
-            return json.as_object().at("ok").as_bool();
+            return json.as_object()
+                .at("ok")
+                .as_bool();
         });
+}
+
+void verify_access_tokens(http_client client, const std::string &access_token, const std::string &bot_access_token)
+{
+    if (access_token.empty())
+    {
+        throw std::runtime_error("Could not get access token. SLACK_BOT_ACCESS_TOKEN not set\n");
+    }
+
+    if (access_token.empty())
+    {
+        throw std::runtime_error("Could not get access token. SLACK_ACCESS_TOKEN not set\n");
+    }
+
+    bool token_result = test_access_token(client, access_token).get();
+    if (!token_result)
+    {
+        throw std::runtime_error("could not authenticate. Please check access token");
+    }
+
+    bool bot_token_result = test_access_token(client, bot_access_token).get();
+    if (!bot_token_result)
+    {
+        throw std::runtime_error("could not authenticate bot. Please check bot access token");
+    }
 }
 
 int main()
 {
-    const char *access_token = std::getenv("SLACK_ACCESS_TOKEN");
-    if (!access_token)
-    {
-        fprintf(stderr, "Could not get access token. SLACK_ACCESS_TOKEN not set\n");
-        return -1;
-    }
-
     try
     {
-        // Create http_client to send the request.
         http_client client(api_url);
 
-        bool result = test_access(client).wait();
-        printf("%s\n", result ? "access okay" : "access not okay");
+        const std::string access_token(std::getenv("SLACK_ACCESS_TOKEN"));
+        const std::string bot_access_token(std::getenv("SLACK_BOT_ACCESS_TOKEN"));
+
+        verify_access_tokens(client, access_token, bot_access_token);
     }
     catch (const std::exception &e)
     {
-        printf("Error exception:%s\n", e.what());
+        fprintf(stderr, "Error:%s\n", e.what());
     }
-    
+
     return 0;
 }
