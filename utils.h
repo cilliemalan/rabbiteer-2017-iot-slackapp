030e38cadf2d7bedb1d6c46e881b3537fdeaaf8e
Cillié Malan <cilliemalan@gmail.com>
2017-10-25 11:16:23 +0200

    Added sequential_for and sequential_transform

--- utils.h
+++ utils.h
@@ -23,3 +23,60 @@ pplx::task<T> task_after(T w, std::chrono::duration<_Rep, _Period> delay) {
 std::string utf16_to_utf8(const std::wstring &w);
 std::wstring utf8_to_utf16(const std::string &w);
 
+
+template<typename _Iter, typename _Func>
+pplx::task<void> sequential_for(_Iter begin, _Iter end, _Func transform)
+{
+    typedef typename std::iterator_traits<_Iter>::value_type _Elem;
+    typedef const typename std::iterator_traits<_Iter>::pointer _PElem;
+    typedef const typename std::iterator_traits<_Iter>::reference _CRef;
+
+    pplx::task<void> last_task = pplx::task_from_result();
+
+    while (begin != end)
+    {
+        _CRef v = *begin;
+        ++begin;
+
+        last_task = last_task.then([&v, transform] {
+            pplx::task<void> transformed = transform(v);
+            return transformed;
+        });
+
+    }
+
+    return last_task;
+}
+
+
+template<typename _Iter, typename _Func>
+auto sequential_transform(_Iter begin, _Iter end, _Func transform)
+-> decltype(pplx::task<std::vector<typename std::iterator_traits<_Iter>::value_type>>())
+{
+    typedef typename std::iterator_traits<_Iter>::value_type _Elem;
+    typedef const typename std::iterator_traits<_Iter>::pointer _PElem;
+    typedef const typename std::iterator_traits<_Iter>::reference _CRef;
+    auto poutput = std::make_shared<std::vector<_Elem>>();
+
+    pplx::task<void> last_task = pplx::task_from_result();
+
+    while (begin != end)
+    {
+        _CRef v = *begin;
+        ++begin;
+
+        last_task = last_task.then([&v, poutput, transform] {
+
+            pplx::task<_Elem> transformed = transform(v);
+
+            return transformed.then([poutput](_Elem result) {
+                poutput->emplace_back(result);
+            });
+        });
+
+    }
+
+    return last_task.then([=] {
+        return std::forward<std::vector<_Elem>>(*poutput);
+    });
+}
